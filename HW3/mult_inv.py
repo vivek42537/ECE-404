8280a741892a3cd7021c3a60cd787f9bd55d17b5
Vivek Shashank Khanolkar <vkhanolk@ecegrid-thin2.ecn.purdue.edu>
2021-02-10 21:20:27 -0500

    done

--- HW3/mult_inv.py
+++ HW3/mult_inv.py
@@ -5,37 +5,68 @@
 # vkhanolk
 # 2/11/2021
 
-#Following code modified from BGCD.py
+#Following code modified from FindMI.py
 
 import sys
-if len(sys.argv) != 3:
-    sys.exit("\nUsage:   %s  <integer>  <integer>\n" % sys.argv[0])
 
-a,b = int(sys.argv[1]),int(sys.argv[2])
+if len(sys.argv) != 3:  
+    sys.stderr.write("Usage: %s   <integer>   <modulus>\n" % sys.argv[0]) 
+    sys.exit(1) 
 
-def bgcd(a,b):
-    if a == b: return a                                         #(A)
-    if a == 0: return b                                         #(B)
-    if b == 0: return a                                         #(C)
-    if (~a & 1):                                                #(D)
-        if (b &1):
-            print("HER") 
-            print("A:", a >> 1, "B:", b)                                            #(E)
-            return bgcd(a >> 1, b)                              #(F)
-        else:
-            print("HERE")                                                   #(G)
-            return bgcd(a >> 1, b >> 1) << 1                    #(H)
-    if (~b & 1):  
-        print("HERE1")                                             #(I)
-        print("A: ", a, "B: ", b >> 1)
-        return bgcd(a, b >> 1)                                  #(J)
-    if (a > b):  
-        print("HERE2") 
-        print("A: ", (a-b) >> 1, "B: ", b)                                                #(K)
-        return bgcd( (a-b) >> 1, b)                             #(L)
-    print("DONE")
-    print("A: ", (b-a) >> 1, "B: ", a)
-    return bgcd( (b-a) >> 1, a )                                #(M)
+NUM, MOD = int(sys.argv[1]), int(sys.argv[2])
 
-gcdval = bgcd(a, b)
-print("\nBGCD: %d\n" % gcdval)
+# utilizing russianPeasant method to bitwise multiply (had to modify slightly 
+# since wasn't working with some negative test cases)
+
+def russianPeasant(a, b):
+    ans = 0
+    if (b < 0) :
+        b = -b
+        a = -a
+    while (b > 0):
+        if (b & 1): #if b is odd
+            ans = ans + a
+
+        a = a << 1
+        b = b >> 1
+    
+    return ans
+
+def divider(x, y):
+    count = 0
+    while (x > y):
+        x -= y
+        count += 1
+
+    return count
+
+def MI(num, mod):
+    '''
+    This function uses ordinary integer arithmetic implementation of the
+    Extended Euclid's Algorithm to find the MI of the first-arg integer
+    vis-a-vis the second-arg integer.
+    '''
+    #print("ANS: ", russianPeasant(-2, -5))
+    #print("DIV: ", divider(15, 2))
+    NUM = num; MOD = mod
+    x, x_old = 0, 1
+    y, y_old = 1, 0
+    while mod:
+        q = divider(num, mod)
+        num, mod = mod, num % mod
+        x, x_old = x_old - russianPeasant(q,x), x
+        y, y_old = y_old - russianPeasant(q,y), y
+        # print("Q: ", q)
+        # print("num: ", num)
+        # print("mod: ", mod)
+        # print("X: ", x, "X_OLD: ", x_old)
+        # print("Y: ", y, "Y_OLD: ", y_old)
+        # print("________________________________")
+    if num != 1:
+        print("\nNO MI. However, the GCD of %d and %d is %u\n" % (NUM, MOD, num))
+    else:
+        MI = (x_old + MOD) % MOD
+        print("\nMI of %d modulo %d is: %d\n" % (NUM, MOD, MI))
+
+MI(NUM, MOD)
+
